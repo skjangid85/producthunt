68109190a03785ea20369f54576c7f8f7bab82fa
Sohan <skjangid85@gmail.com>
2021-02-12 17:54:35 +0530

    project completed

--- products/views.py
+++ products/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Products
 from django.utils import timezone
 
 def home(request):
-    return render(request, 'products/home.html')
+    products = Products.objects
+    return render(request, 'products/home.html', {'products': products})
 
 
-@login_required
+@login_required(login_url = '/accounts/signup')
 def create(request):
     if request.method == 'POST':
         if request.POST['title'] and request.POST['body'] and request.FILES['icon'] and request.POST['url'] and request.FILES['image']:
@@ -23,8 +24,21 @@ def create(request):
             product.pub_date = timezone.datetime.now()
             product.hunter = request.user
             product.save()
-            return redirect('home')
+            return redirect('/products/' + str(product.id))
         else:
             return render(request, 'products/create.html', {'error': 'all fields are required'})
     else:
         return render(request, 'products/create.html')
+
+def detail(request, product_id):
+    product = get_object_or_404(Products, pk=product_id)
+    return render(request, 'products/detail.html', {'product': product})
+
+
+@login_required(login_url = '/accounts/signup')
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Products, pk=product_id)
+        product.votes_total += 1
+        product.save()
+        return redirect('/products/' + str(product.id))
